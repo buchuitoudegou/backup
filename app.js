941a59082f9e48a2e9fa3c3da7a8af9249e338f9
unknown <756541536@qq.com>
2018-12-31 14:09:02 +0800

    intial

---
+++ app.js
@@ -0,0 +1,10 @@
+const Koa = require('koa');
+const static = require('koa-static');
+const path = require('path') 
+const app = new Koa();
+const root = 'dist';
+app.use(static(
+  path.join(__dirname, root)
+));
+
+app.listen(3000);
