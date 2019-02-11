f5678757f322d17a93915de1c19d01665bf87e69
buchuitoudegou <756541536@qq.com>
2019-02-11 17:03:50 +0800

    dist2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const Koa = require('koa');
 const static = require('koa-static');
 const path = require('path') 
 const app = new Koa();
-const root = 'dist';
+const root = 'dist2';
 app.use(static(
   path.join(__dirname, root)
 ));
